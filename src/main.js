0f85748b5d082784710fd3099ed19a2477520077
Benjamin Caradeuc <bencrdc@gmail.com>
2018-06-15 00:35:17 +0200

    Better way of initialising firebase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
 
 const config = {
   apiKey: 'AIzaSyB_TZzRnGXSH-xvPS8hpdqXOcnnVhPKeaM',
