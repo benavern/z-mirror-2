68e7e1b3e6ace66e999820c399bc9a5c2d1f770f
Benjamin Caradeuc <bencrdc@gmail.com>
2018-06-14 22:49:34 +0200

    Add $firebase in Vue context

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyB_TZzRnGXSH-xvPS8hpdqXOcnnVhPKeaM',
+  authDomain: 'z-mirror-2.firebaseapp.com',
+  databaseURL: 'https://z-mirror-2.firebaseio.com',
+  projectId: 'z-mirror-2',
+  storageBucket: 'z-mirror-2.appspot.com',
+  messagingSenderId: '721220130430'
+}
+
+Vue.prototype.$firebase = firebase.initializeApp(config).database()
 
 Vue.config.productionTip = false
 
