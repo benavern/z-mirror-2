4df8a30ec28ede255232a534101930b49e9626f0
Benjamin Caradeuc <bencrdc@gmail.com>
2018-06-23 00:05:41 +0200

    Add admin home logic

--- src/views/admin/Index.vue
+++ src/views/admin/Index.vue
@@ -1,6 +1,53 @@
 <template>
-  <div>
-    <h1>Index</h1>
-    <p>This is the admin home</p>
+  <div id="admin-index" class="section columns is-multiline">
+    <div class="column" v-for="(page, index) in pages" :key="index">
+      <div class="card">
+        <div @click="$router.go(page.path)" class="card-content">
+          <div class="media">
+            <div class="media-left" v-if="page.icon">
+              <b-icon :icon="page.icon" size="is-large" />
+            </div>
+
+            <div class="media-content">
+              <p class="title is-4">{{ page.title }}</p>
+              <p class="subtitle is-6">{{ page.subtitle }}</p>
+            </div>
+          </div>
+
+          <div class="content">
+            {{ page.content }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'adminIndex',
+  data () {
+    return {
+      pages: [
+        {
+          path: '/admin/shopping',
+          icon: 'basket-fill',
+          title: 'Liste de courses',
+          subtitle: '',
+          content: 'Pour ne jamais se retrouver le frigo vide.'
+        }
+      ]
+    }
+  },
+  mounted () {
+    // dynamic shopping list count
+    this.$data.pages[0].subtitle = this.nbShoppingItems
+  },
+  computed: {
+    nbShoppingItems () {
+      const nb = this.$store.getters['shopping/items'].length || 0
+      return `(${nb} ${nb > 1 ? ' articles' : ' article'})`
+    }
+  }
+}
+</script>
