c2d5056bb51c5e4d1442b5105724f827b633825b
Benjamin Caradeuc <bencrdc@gmail.com>
2018-06-22 23:05:00 +0200

    Add container on navbar for desktop

--- src/components/admin/Navigation.vue
+++ src/components/admin/Navigation.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="navbar is-fixed-top is-light">
-    <div class="navbar-brand">
-      <router-link to="/admin" class="navbar-item">
-        <img src="../../assets/icon.png" alt="Z-mirror, a mirror with super power">
-      </router-link>
-
-      <h1 class="navbar-item page-title">
-        {{ $route.meta.displayName || 'Z-Mirror 2' }}
-      </h1>
-
-      <a class="navbar-burger" @click.prevent="toggleMenu">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div :class="['navbar-menu', {'is-active': menuVisible}]" @click="toggleMenu">
-      <div class="navbar-end">
-        <router-link to="/admin/shopping" class="navbar-item">
-          Liste de courses
+    <div class="container">
+      <div class="navbar-brand">
+        <router-link to="/admin" class="navbar-item">
+          <img src="../../assets/icon.png" alt="Z-mirror, a mirror with super power">
         </router-link>
 
-        <div class="navbar-item" @click="logout">
-          Déconnection
+        <h1 class="navbar-item page-title">
+          {{ $route.meta.displayName || 'Z-Mirror 2' }}
+        </h1>
+
+        <a class="navbar-burger" @click.prevent="toggleMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div :class="['navbar-menu', {'is-active': menuVisible}]" @click="toggleMenu">
+        <div class="navbar-end">
+          <router-link to="/admin/shopping" class="navbar-item">
+            Liste de courses
+          </router-link>
+
+          <div class="navbar-item" @click="logout">
+            Déconnection
+          </div>
         </div>
       </div>
     </div>
@@ -40,13 +42,13 @@ export default {
     }
   },
   mounted () {
-    // debugger
     this.routes = this.$router.options.routes
   },
   methods: {
     toggleMenu () {
       this.menuVisible = !this.menuVisible
     },
+
     logout () {
       sessionStorage.removeItem('authenticated')
       this.$router.go('/admin/login')
