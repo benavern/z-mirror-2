6fa3ff8374601f2052def6091db6b70eb4703cbb
Benjamin Caradeuc <bencrdc@gmail.com>
2018-06-22 23:21:49 +0200

    No more need to add firebase database in Vue context

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import firebaseDb from './firebase'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
 import '@mdi/font/css/materialdesignicons.css'
 import store from './store'
 
 Vue.use(Buefy)
-
-Vue.prototype.$firebase = firebaseDb
 
 Vue.config.productionTip = false
 
